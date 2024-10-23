5be8b10b2dd78f47ae30d2b66e224151edb7c55b
Bhargava784282 <bhargava@rahasinteractive.com>
2024-10-23 20:02:49 +0530

    First push and final push with chest system running

---
+++ Assets/Scripts/ChestOpenModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestOpenModel : MonoBehaviour
+{
+    public Button startTimerbtn;
+    public Button openWithGemsBtn;
+    public Button CloseBtn;
+}
